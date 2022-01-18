llvm/llvm-project

[sanitizer] Implement MapDynamicShadowAndAliases.
The function works like MapDynamicShadow, except that it creates aliased
memory to the right of the shadow.  The main use case is for HWASan
aliasing mode, which gets fast IsAlias() checks by exploiting the fact
that the upper bits of the shadow base and aliased memory match.

Reviewed By: vitalybuka, eugenis

Differential Revision: https://reviews.llvm.org/D98369

compiler-rt/lib/sanitizer_common/sanitizer_mac.cpp
@@ -142,6 +142,12 @@ uptr internal_munmap(void *addr, uptr length) {
   return munmap(addr, length);
 }
 
+uptr internal_mremap(void *old_address, uptr old_size, uptr new_size, int flags,
+                     void *new_address) {
+  CHECK(false && "internal_mremap is unimplemented on Mac");
+  return 0;
+}
+
 int internal_mprotect(void *addr, uptr length, int prot) {
   return mprotect(addr, length, prot);
 }
@@ -1250,6 +1256,12 @@ uptr MapDynamicShadow(uptr shadow_size_bytes, uptr shadow_scale,
   CHECK_NE((uptr)0, shadow_start);
   CHECK(IsAligned(shadow_start, alignment));
   return shadow_start;
+}
+
+uptr MapDynamicShadowAndAliases(uptr shadow_size, uptr alias_size,
+                                uptr num_aliases, uptr ring_buffer_size) {
+  CHECK(false && "HWASan aliasing is unimplemented on Mac");
+  return 0;
 }
 
 uptr FindAvailableMemoryRange(uptr size, uptr alignment, uptr left_padding,

compiler-rt/lib/sanitizer_common/sanitizer_posix.h
@@ -40,6 +40,8 @@ uptr internal_write(fd_t fd, const void *buf, uptr count);
 uptr internal_mmap(void *addr, uptr length, int prot, int flags,
                    int fd, u64 offset);
 uptr internal_munmap(void *addr, uptr length);
+uptr internal_mremap(void *old_address, uptr old_size, uptr new_size, int flags,
+                     void *new_address);
 int internal_mprotect(void *addr, uptr length, int prot);
 int internal_madvise(uptr addr, uptr length, int advice);
 

CTSRD-CHERI/compiler-rt

lib/sanitizer_common/sanitizer_mac.cpp
@@ -142,7 +142,13 @@ usize internal_munmap(void *addr, usize length) {
   return munmap(addr, length);
 }
 
-int internal_mprotect(void *addr, usize length, int prot) {
+uptr internal_mremap(void *old_address, usize old_size, usize new_size, int flags,
+                     void *new_address) {
+  CHECK(false && "internal_mremap is unimplemented on Mac");
+  return 0;
+}
+
+int internal_mprotect(void *addr, uptr length, int prot) {
   return mprotect(addr, length, prot);
 }
 
@@ -1250,6 +1256,12 @@ uptr MapDynamicShadow(uptr shadow_size_bytes, uptr shadow_scale,
   CHECK_NE((uptr)0, shadow_start);
   CHECK(IsAligned(shadow_start, alignment));
   return shadow_start;
+}
+
+uptr MapDynamicShadowAndAliases(usize shadow_size, usize alias_size,
+                                usize num_aliases, usize ring_buffer_size) {
+  CHECK(false && "HWASan aliasing is unimplemented on Mac");
+  return 0;
 }
 
 uptr FindAvailableMemoryRange(usize size, usize alignment, usize left_padding,

lib/sanitizer_common/sanitizer_posix.h
@@ -40,6 +40,8 @@ usize internal_write(fd_t fd, const void *buf, usize count);
 uptr internal_mmap(void *addr, usize length, int prot, int flags,
                    int fd, u64 offset);
 uptr internal_munmap(void *addr, usize length);
+uptr internal_mremap(void *old_address, usize old_size, usize new_size, int flags,
+                     void *new_address);
 int internal_mprotect(void *addr, usize length, int prot);
 int internal_madvise(uptr addr, usize length, int advice);
 