jove-decompiler/llvm-project

sanitizer_common: Introduce internal_madvise and start using it.
A recent change to sanitizer_common caused us to issue the syscall
madvise(MADV_HUGEPAGE) during HWASAN initialization. This may lead to a
problem if madvise is instrumented (e.g. because libc is instrumented
or the user intercepted it). For example, on Android the syscall may
fail if the kernel does not support transparent hugepages, which leads
to an attempt to set errno in a HWASAN instrumented function. Avoid
this problem by introducing a syscall wrapper and using it to issue
this syscall.

Tested only on Linux; includes untested updates for the other
platforms.

Differential Revision: https://reviews.llvm.org/D85870

compiler-rt/lib/sanitizer_common/sanitizer_mac.cpp
@@ -137,6 +137,10 @@ int internal_mprotect(void *addr, uptr length, int prot) {
   return mprotect(addr, length, prot);
 }
 
+int internal_madvise(uptr addr, uptr length, int advice) {
+  return madvise((void *)addr, length, advice);
+}
+
 uptr internal_close(fd_t fd) {
   return close(fd);
 }

compiler-rt/lib/sanitizer_common/sanitizer_posix.h
@@ -42,6 +42,7 @@ uptr internal_mmap(void *addr, uptr length, int prot, int flags,
                    int fd, u64 offset);
 uptr internal_munmap(void *addr, uptr length);
 int internal_mprotect(void *addr, uptr length, int prot);
+int internal_madvise(uptr addr, uptr length, int advice);
 
 // OS
 uptr internal_filesize(fd_t fd);  // -1 on error.

CTSRD-CHERI/compiler-rt

lib/sanitizer_common/sanitizer_mac.cpp
@@ -137,6 +137,10 @@ int internal_mprotect(void *addr, usize length, int prot) {
   return mprotect(addr, length, prot);
 }
 
+int internal_madvise(uptr addr, usize length, int advice) {
+  return madvise((void *)addr, length, advice);
+}
+
 usize internal_close(fd_t fd) {
   return close(fd);
 }

lib/sanitizer_common/sanitizer_posix.h
@@ -42,6 +42,7 @@ uptr internal_mmap(void *addr, usize length, int prot, int flags,
                    int fd, u64 offset);
 uptr internal_munmap(void *addr, usize length);
 int internal_mprotect(void *addr, usize length, int prot);
+int internal_madvise(uptr addr, usize length, int advice);
 
 // OS
 usize internal_filesize(fd_t fd);  // -1 on error.