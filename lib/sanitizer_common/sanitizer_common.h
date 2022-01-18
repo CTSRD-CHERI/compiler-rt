llvm/llvm-project

[asan] Add %d variable to external_symbolizer_path option, so that user can specify paths relative to the location of the binary.
We want way to set a path to llvm-symbolizer that isn't relative
to the current working directory; this change adds a variable that
expands to the path relative to the current binary.
This approach came from comments in https://reviews.llvm.org/D93070

Differential Revision: https://reviews.llvm.org/D94563

compiler-rt/lib/sanitizer_common/sanitizer_common.h
@@ -248,6 +248,7 @@ const char *StripModuleName(const char *module);
 // OS
 uptr ReadBinaryName(/*out*/char *buf, uptr buf_len);
 uptr ReadBinaryNameCached(/*out*/char *buf, uptr buf_len);
+uptr ReadBinaryDir(/*out*/ char *buf, uptr buf_len);
 uptr ReadLongProcessName(/*out*/ char *buf, uptr buf_len);
 const char *GetProcessName();
 void UpdateProcessName();

CTSRD-CHERI/compiler-rt

lib/sanitizer_common/sanitizer_common.h
@@ -248,6 +248,7 @@ const char *StripModuleName(const char *module);
 // OS
 usize ReadBinaryName(/*out*/char *buf, usize buf_len);
 usize ReadBinaryNameCached(/*out*/char *buf, usize buf_len);
+usize ReadBinaryDir(/*out*/ char *buf, usize buf_len);
 usize ReadLongProcessName(/*out*/ char *buf, usize buf_len);
 const char *GetProcessName();
 void UpdateProcessName();