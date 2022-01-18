NanQin555/llvm-project

[NFC][Asan] Set AP32::kMetadataSize to 0
Asan does not use metadata with primary allocators.
It should match AP64::kMetadataSize whic is 0.

Depends on D86917.

Reviewed By: morehouse

Differential Revision: https://reviews.llvm.org/D86919

compiler-rt/lib/asan/asan_allocator.h
@@ -171,7 +171,7 @@ template <typename AddressSpaceViewTy>
 struct AP32 {
   static const uptr kSpaceBeg = 0;
   static const u64 kSpaceSize = SANITIZER_MMAP_RANGE_SIZE;
-  static const uptr kMetadataSize = 16;
+  static const uptr kMetadataSize = 0;
   typedef __asan::SizeClassMap SizeClassMap;
   static const uptr kRegionSizeLog = 20;
   using AddressSpaceView = AddressSpaceViewTy;

CTSRD-CHERI/compiler-rt

lib/asan/asan_allocator.h
@@ -171,7 +171,7 @@ template <typename AddressSpaceViewTy>
 struct AP32 {
   static const vaddr kSpaceBeg = 0;
   static const u64 kSpaceSize = SANITIZER_MMAP_RANGE_SIZE;
-  static const usize kMetadataSize = 16;
+  static const usize kMetadataSize = 0;
   typedef __asan::SizeClassMap SizeClassMap;
   static const usize kRegionSizeLog = 20;
   using AddressSpaceView = AddressSpaceViewTy;