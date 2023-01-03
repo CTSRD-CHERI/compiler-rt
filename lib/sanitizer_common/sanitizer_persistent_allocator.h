llvm/llvm-project

[sanitizer] Remove SpinMutexLock from stat function
Mapped size is more appropriate for most users.

Depends on D111605.

Reviewed By: dvyukov

Differential Revision: https://reviews.llvm.org/D111607

compiler-rt/lib/sanitizer_common/sanitizer_persistent_allocator.h
@@ -24,11 +24,7 @@ template <typename T>
 class PersistentAllocator {
  public:
   T *alloc(uptr count = 1);
-  uptr allocated() const {
-    SpinMutexLock l(&mtx);
-    return atomic_load_relaxed(&mapped_size) +
-           atomic_load_relaxed(&region_pos) - atomic_load_relaxed(&region_end);
-  }
+  uptr allocated() const { return atomic_load_relaxed(&mapped_size); }
 
  private:
   T *tryAlloc(uptr count);

CTSRD-CHERI/compiler-rt

lib/sanitizer_common/sanitizer_persistent_allocator.h
@@ -24,11 +24,7 @@ template <typename T>
 class PersistentAllocator {
  public:
   T *alloc(usize count = 1);
-  usize allocated() const {
-    SpinMutexLock l(&mtx);
-    return atomic_load_relaxed(&mapped_size) +
-           atomic_load_relaxed(&region_pos) - atomic_load_relaxed(&region_end);
-  }
+  usize allocated() const { return atomic_load_relaxed(&mapped_size); }
 
  private:
   T *tryAlloc(usize count);