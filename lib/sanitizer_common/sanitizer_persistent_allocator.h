NanQin555/llvm-project

[NFC][sanitizer] Remove global PersistentAllocator
This way is easier to track memory usage and do other
incremental refactorings.

Differential Revision: https://reviews.llvm.org/D111256

compiler-rt/lib/sanitizer_common/sanitizer_persistent_allocator.h
@@ -23,13 +23,19 @@ namespace __sanitizer {
 class PersistentAllocator {
  public:
   void *alloc(uptr size);
+  uptr allocated() const {
+    SpinMutexLock l(&mtx);
+    return atomic_load_relaxed(&mapped_size) +
+           atomic_load_relaxed(&region_pos) - atomic_load_relaxed(&region_end);
+  }
 
  private:
   void *tryAlloc(uptr size);
   void *refillAndAlloc(uptr size);
-  StaticSpinMutex mtx;  // Protects alloc of new blocks for region allocator.
+  mutable StaticSpinMutex mtx;  // Protects alloc of new blocks.
   atomic_uintptr_t region_pos;  // Region allocator for Node's.
   atomic_uintptr_t region_end;
+  atomic_uintptr_t mapped_size;
 };
 
 inline void *PersistentAllocator::tryAlloc(uptr size) {
@@ -49,11 +55,6 @@ inline void *PersistentAllocator::alloc(uptr size) {
   void *s = tryAlloc(size);
   if (s) return s;
   return refillAndAlloc(size);
-}
-
-extern PersistentAllocator thePersistentAllocator;
-inline void *PersistentAlloc(uptr sz) {
-  return thePersistentAllocator.alloc(sz);
 }
 
 } // namespace __sanitizer

compiler-rt/lib/sanitizer_common/sanitizer_stackdepot.cpp
@@ -14,9 +14,12 @@
 
 #include "sanitizer_common.h"
 #include "sanitizer_hash.h"
+#include "sanitizer_persistent_allocator.h"
 #include "sanitizer_stackdepotbase.h"
 
 namespace __sanitizer {
+
+static PersistentAllocator allocator;
 
 struct StackDepotNode {
   using hash_type = u64;
@@ -36,8 +39,10 @@ struct StackDepotNode {
   bool eq(hash_type hash, const args_type &args) const {
     return hash == stack_hash;
   }
-  static uptr storage_size(const args_type &args) {
-    return sizeof(StackDepotNode) + (args.size - 1) * sizeof(uptr);
+  static uptr allocated() { return allocator.allocated(); }
+  static StackDepotNode *allocate(const args_type &args) {
+    uptr alloc_size = sizeof(StackDepotNode) + (args.size - 1) * sizeof(uptr);
+    return (StackDepotNode *)allocator.alloc(alloc_size);
   }
   static hash_type hash(const args_type &args) {
     MurMur2Hash64Builder H(args.size * sizeof(uptr));

CTSRD-CHERI/compiler-rt

lib/sanitizer_common/sanitizer_persistent_allocator.h
@@ -23,13 +23,19 @@ namespace __sanitizer {
 class PersistentAllocator {
  public:
   void *alloc(usize size);
+  usize allocated() const {
+    SpinMutexLock l(&mtx);
+    return atomic_load_relaxed(&mapped_size) +
+           atomic_load_relaxed(&region_pos) - atomic_load_relaxed(&region_end);
+  }
 
  private:
   void *tryAlloc(usize size);
   void *refillAndAlloc(usize size);
-  StaticSpinMutex mtx;  // Protects alloc of new blocks for region allocator.
+  mutable StaticSpinMutex mtx;  // Protects alloc of new blocks.
   atomic_uintptr_t region_pos;  // Region allocator for Node's.
   atomic_uintptr_t region_end;
+  atomic_size_t mapped_size;
 };
 
 inline void *PersistentAllocator::tryAlloc(usize size) {
@@ -49,11 +55,6 @@ inline void *PersistentAllocator::alloc(usize size) {
   void *s = tryAlloc(size);
   if (s) return s;
   return refillAndAlloc(size);
-}
-
-extern PersistentAllocator thePersistentAllocator;
-inline void *PersistentAlloc(usize sz) {
-  return thePersistentAllocator.alloc(sz);
 }
 
 } // namespace __sanitizer

lib/sanitizer_common/sanitizer_stackdepot.cpp
@@ -14,9 +14,12 @@
 
 #include "sanitizer_common.h"
 #include "sanitizer_hash.h"
+#include "sanitizer_persistent_allocator.h"
 #include "sanitizer_stackdepotbase.h"
 
 namespace __sanitizer {
+
+static PersistentAllocator allocator;
 
 struct StackDepotNode {
   using hash_type = u64;
@@ -36,8 +39,10 @@ struct StackDepotNode {
   bool eq(hash_type hash, const args_type &args) const {
     return hash == stack_hash;
   }
-  static usize storage_size(const args_type &args) {
-    return sizeof(StackDepotNode) + (args.size - 1) * sizeof(args.trace[0]);
+  static usize allocated() { return allocator.allocated(); }
+  static StackDepotNode *allocate(const args_type &args) {
+    uptr alloc_size = sizeof(StackDepotNode) + (args.size - 1) * sizeof(args.trace[0]);
+    return (StackDepotNode *)allocator.alloc(alloc_size);
   }
   static hash_type hash(const args_type &args) {
     MurMur2Hash64Builder H(args.size * sizeof(args.trace[0]));