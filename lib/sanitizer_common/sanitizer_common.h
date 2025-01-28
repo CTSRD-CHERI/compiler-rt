NanQin555/llvm-project

[Sanitizers] UUID/Build ID is 16 bytes on Darwin.
https://reviews.llvm.org/D114294 changed the kModuleUUIDSize from 16 to 32 for
all platforms. This is not true for Darwin where the UUID is 16 bytes.

Differential Revision: https://reviews.llvm.org/D152309
rdar://110346025

compiler-rt/lib/sanitizer_common/sanitizer_common.h
@@ -796,7 +796,11 @@ inline const char *ModuleArchToString(ModuleArch arch) {
   return "";
 }
 
+#if SANITIZER_APPLE
+const uptr kModuleUUIDSize = 16;
+#else
 const uptr kModuleUUIDSize = 32;
+#endif
 const uptr kMaxSegName = 16;
 
 // Represents a binary loaded into virtual memory (e.g. this can be an

CTSRD-CHERI/compiler-rt

lib/sanitizer_common/sanitizer_common.h
@@ -854,7 +854,11 @@ inline const char *ModuleArchToString(ModuleArch arch) {
   return "";
 }
 
+#if SANITIZER_APPLE
+const usize kModuleUUIDSize = 16;
+#else
 const usize kModuleUUIDSize = 32;
+#endif
 const usize kMaxSegName = 16;
 
 // Represents a binary loaded into virtual memory (e.g. this can be an