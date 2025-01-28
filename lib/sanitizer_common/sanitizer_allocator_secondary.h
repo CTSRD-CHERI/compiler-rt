llvm/llvm-project

[NFC][sanitizer] Pass user region into OnMapSecondary

compiler-rt/lib/sanitizer_common/sanitizer_allocator_secondary.h
@@ -82,7 +82,7 @@ class LargeMmapAllocator {
     InitLinkerInitialized();
   }
 
-  void *Allocate(AllocatorStats *stat, uptr size, uptr alignment) {
+  void *Allocate(AllocatorStats *stat, const uptr size, uptr alignment) {
     CHECK(IsPowerOfTwo(alignment));
     uptr map_size = RoundUpMapSize(size);
     if (alignment > page_size_)
@@ -99,11 +99,11 @@ class LargeMmapAllocator {
     if (!map_beg)
       return nullptr;
     CHECK(IsAligned(map_beg, page_size_));
-    MapUnmapCallback().OnMapSecondary(map_beg, map_size);
     uptr map_end = map_beg + map_size;
     uptr res = map_beg + page_size_;
     if (res & (alignment - 1))  // Align.
       res += alignment - (res & (alignment - 1));
+    MapUnmapCallback().OnMapSecondary(map_beg, map_size, res, size);
     CHECK(IsAligned(res, alignment));
     CHECK(IsAligned(res, page_size_));
     CHECK_GE(res + size, map_beg);

compiler-rt/lib/sanitizer_common/tests/sanitizer_allocator_test.cpp
@@ -478,7 +478,10 @@ TEST(SanitizerCommon, SizeClassAllocator32CompactGetBlockBegin) {
 struct TestMapUnmapCallback {
   static int map_count, map_secondary_count, unmap_count;
   void OnMap(uptr p, uptr size) const { map_count++; }
-  void OnMapSecondary(uptr p, uptr size) const { map_secondary_count++; }
+  void OnMapSecondary(uptr p, uptr size, uptr user_begin,
+                      uptr user_size) const {
+    map_secondary_count++;
+  }
   void OnUnmap(uptr p, uptr size) const { unmap_count++; }
 
   static void Reset() { map_count = map_secondary_count = unmap_count = 0; }

CTSRD-CHERI/compiler-rt

lib/sanitizer_common/sanitizer_allocator_secondary.h
@@ -82,7 +82,7 @@ class LargeMmapAllocator {
     InitLinkerInitialized();
   }
 
-  void *Allocate(AllocatorStats *stat, usize size, usize alignment) {
+  void *Allocate(AllocatorStats *stat, const usize size, usize alignment) {
     CHECK(IsPowerOfTwo(alignment));
     usize map_size = RoundUpMapSize(size);
     if (alignment > page_size_)
@@ -99,11 +99,11 @@ class LargeMmapAllocator {
     if (!map_beg)
       return nullptr;
     CHECK(IsAligned(map_beg, page_size_));
-    MapUnmapCallback().OnMapSecondary(map_beg, map_size);
     uptr map_end = map_beg + map_size;
     uptr res = map_beg + page_size_;
     if (!IsAligned(res, alignment))  // Align.
       res = RoundUpTo(res, alignment);
+    MapUnmapCallback().OnMapSecondary(map_beg, map_size, res, size);
     CHECK(IsAligned(res, alignment));
     CHECK(IsAligned(res, page_size_));
     CHECK_GE(res + size, map_beg);

lib/sanitizer_common/tests/sanitizer_allocator_test.cpp
@@ -478,7 +478,10 @@ TEST(SanitizerCommon, SizeClassAllocator32CompactGetBlockBegin) {
 struct TestMapUnmapCallback {
   static int map_count, map_secondary_count, unmap_count;
   void OnMap(uptr p, usize size) const { map_count++; }
-  void OnMapSecondary(uptr p, usize size) const { map_secondary_count++; }
+  void OnMapSecondary(uptr p, usize size, uptr user_begin,
+                      usize user_size) const {
+    map_secondary_count++;
+  }
   void OnUnmap(uptr p, usize size) const { unmap_count++; }
 
   static void Reset() { map_count = map_secondary_count = unmap_count = 0; }