llvm/llvm-project

[NFC] Add MprotectReadWrite
It's unused, but I need it for debuging.
Seems usefull for completeness.

Reviewed By: thurston

Differential Revision: https://reviews.llvm.org/D156180

compiler-rt/lib/sanitizer_common/sanitizer_common.h
@@ -117,6 +117,7 @@ void *MmapAlignedOrDieOnFatalError(uptr size, uptr alignment,
 // unaccessible memory.
 bool MprotectNoAccess(uptr addr, uptr size);
 bool MprotectReadOnly(uptr addr, uptr size);
+bool MprotectReadWrite(uptr addr, uptr size);
 
 void MprotectMallocZones(void *addr, int prot);
 

CTSRD-CHERI/compiler-rt

lib/sanitizer_common/sanitizer_common.h
@@ -117,6 +117,7 @@ void *MmapAlignedOrDieOnFatalError(usize size, usize alignment,
 // unaccessible memory.
 bool MprotectNoAccess(uptr addr, usize size);
 bool MprotectReadOnly(uptr addr, usize size);
+bool MprotectReadWrite(uptr addr, usize size);
 
 void MprotectMallocZones(void *addr, int prot);
 