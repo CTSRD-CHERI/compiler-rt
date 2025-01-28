llvm/llvm-project

Revert "[compiler-rt] Avoid memintrinsic calls inserted by the compiler"
This reverts commit e614d5667f6c6fc6c645587cb9aee1a058285454.

Build bot failures:

| FAILED: lib/clang/17/lib/linux/libclang_rt.ubsan_minimal-i386.so
| : && /usr/bin/clang++ -fPIC -fPIC -fno-semantic-interposition -fvisibility-inlines-hidden -Werror=date-time -Werror=unguarded-availability-new -Wall -Wextra -Wno-unused-parameter -Wwrite-strings -Wcast-qual -Wmissing-field-initializers -pedantic -Wno-long-long -Wc++98-compat-extra-semi -Wimplicit-fallthrough -Wcovered-switch-default -Wno-noexcept-type -Wnon-virtual-dtor -Wdelete-non-virtual-dtor -Wsuggest-override -Wstring-conversion -Wmisleading-indentation -Wctad-maybe-unsupported -fdiagnostics-color -ffunction-sections -fdata-sections -Wall -Wno-unused-parameter -O3 -DNDEBUG  -Wl,-z,defs -Wl,-z,nodelete   -m32 -nodefaultlibs -Wl,-z,text -nostdlib++ -shared -Wl,-soname,libclang_rt.ubsan_minimal-i386.so -o lib/clang/17/lib/linux/libclang_rt.ubsan_minimal-i386.so projects/compiler-rt/lib/ubsan_minimal/CMakeFiles/RTUbsan_minimal.i386.dir/ubsan_minimal_handlers.cpp.o  -lgcc_s  -lc && :
| /usr/bin/ld: projects/compiler-rt/lib/ubsan_minimal/CMakeFiles/RTUbsan_minimal.i386.dir/ubsan_minimal_handlers.cpp.o: in function `__ubsan_handle_type_mismatch_minimal':
| /b/sanitizer-x86_64-linux-bootstrap-msan/build/llvm-project/compiler-rt/lib/ubsan_minimal/ubsan_minimal_handlers.cpp:120: undefined reference to `__sanitizer_internal_memcpy'
| /usr/bin/ld: projects/compiler-rt/lib/ubsan_minimal/CMakeFiles/RTUbsan_minimal.i386.dir/ubsan_minimal_handlers.cpp.o: in function `__ubsan_handle_type_mismatch_minimal_abort':
| /b/sanitizer-x86_64-linux-bootstrap-msan/build/llvm-project/compiler-rt/lib/ubsan_minimal/ubsan_minimal_handlers.cpp:120: undefined reference to `__sanitizer_internal_memcpy'
| /usr/bin/ld: projects/compiler-rt/lib/ubsan_minimal/CMakeFiles/RTUbsan_minimal.i386.dir/ubsan_minimal_handlers.cpp.o: in function `__ubsan_handle_alignment_assumption_mini

compiler-rt/lib/sanitizer_common/sanitizer_libc.cpp
@@ -10,9 +10,6 @@
 // run-time libraries. See sanitizer_libc.h for details.
 //===----------------------------------------------------------------------===//
 
-// Do not redefine builtins; this file is defining the builtin replacements.
-#define SANITIZER_COMMON_NO_REDEFINE_BUILTINS
-
 #include "sanitizer_allocator_internal.h"
 #include "sanitizer_common.h"
 #include "sanitizer_libc.h"
@@ -49,11 +46,7 @@ int internal_memcmp(const void* s1, const void* s2, uptr n) {
   return 0;
 }
 
-void *internal_memcpy(void *dest, const void *src, uptr n)
-    ALIAS(__sanitizer_internal_memcpy);
-SANITIZER_INTERFACE_ATTRIBUTE
-extern "C" void *__sanitizer_internal_memcpy(void *dest, const void *src,
-                                             uptr n) {
+void *internal_memcpy(void *dest, const void *src, uptr n) {
   char *d = (char*)dest;
   const char *s = (const char *)src;
   for (uptr i = 0; i < n; ++i)
@@ -61,11 +54,7 @@ extern "C" void *__sanitizer_internal_memcpy(void *dest, const void *src,
   return dest;
 }
 
-void *internal_memmove(void *dest, const void *src, uptr n)
-    ALIAS(__sanitizer_internal_memmove);
-SANITIZER_INTERFACE_ATTRIBUTE
-extern "C" void *__sanitizer_internal_memmove(void *dest, const void *src,
-                                              uptr n) {
+void *internal_memmove(void *dest, const void *src, uptr n) {
   char *d = (char*)dest;
   const char *s = (const char *)src;
   sptr i, signed_n = (sptr)n;
@@ -83,10 +72,7 @@ extern "C" void *__sanitizer_internal_memmove(void *dest, const void *src,
   return dest;
 }
 
-void *internal_memset(void *s, int c, uptr n)
-    ALIAS(__sanitizer_internal_memset);
-SANITIZER_INTERFACE_ATTRIBUTE
-extern "C" void *__sanitizer_internal_memset(void *s, int c, uptr n) {
+void *internal_memset(void* s, int c, uptr n) {
   // Optimize for the most performance-critical case:
   if ((reinterpret_cast<uptr>(s) % 16) == 0 && (n % 16) == 0) {
     u64 *p = reinterpret_cast<u64*>(s);

CTSRD-CHERI/compiler-rt

lib/sanitizer_common/sanitizer_libc.cpp
@@ -10,9 +10,6 @@
 // run-time libraries. See sanitizer_libc.h for details.
 //===----------------------------------------------------------------------===//
 
-// Do not redefine builtins; this file is defining the builtin replacements.
-#define SANITIZER_COMMON_NO_REDEFINE_BUILTINS
-
 #include "sanitizer_allocator_internal.h"
 #include "sanitizer_common.h"
 #include "sanitizer_libc.h"
@@ -61,11 +58,7 @@ void *internal_memmove(void *dest, const void *src, usize n) {
   return memmove(dest, src, n);
 }
 #else
-void *internal_memcpy(void *dest, const void *src, usize n)
-    ALIAS(__sanitizer_internal_memcpy);
-SANITIZER_INTERFACE_ATTRIBUTE
-extern "C" void *__sanitizer_internal_memcpy(void *dest, const void *src,
-                                             usize n) {
+void *internal_memcpy(void *dest, const void *src, usize n) {
   char *d = (char*)dest;
   const char *s = (const char *)src;
   for (usize i = 0; i < n; ++i)
@@ -73,11 +66,7 @@ extern "C" void *__sanitizer_internal_memcpy(void *dest, const void *src,
   return dest;
 }
 
-void *internal_memmove(void *dest, const void *src, usize n)
-    ALIAS(__sanitizer_internal_memmove);
-SANITIZER_INTERFACE_ATTRIBUTE
-extern "C" void *__sanitizer_internal_memmove(void *dest, const void *src,
-                                              usize n) {
+void *internal_memmove(void *dest, const void *src, usize n) {
   char *d = (char*)dest;
   const char *s = (const char *)src;
   sptr i, signed_n = (sptr)n;
@@ -96,10 +85,7 @@ extern "C" void *__sanitizer_internal_memmove(void *dest, const void *src,
 }
 #endif
 
-void *internal_memset(void *s, int c, usize n)
-    ALIAS(__sanitizer_internal_memset);
-SANITIZER_INTERFACE_ATTRIBUTE
-extern "C" void *__sanitizer_internal_memset(void *s, int c, usize n) {
+void *internal_memset(void* s, int c, usize n) {
   // Optimize for the most performance-critical case:
   if (IsAligned(s, 16) && (n % 16) == 0) {
     u64 *p = reinterpret_cast<u64*>(s);