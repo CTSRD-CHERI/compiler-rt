jove-decompiler/llvm-project

[test][sanitizer] Fix test on windows

compiler-rt/lib/sanitizer_common/tests/sanitizer_array_ref_test.cpp
@@ -121,7 +121,7 @@ TEST(ArrayRefTest, ArrayRef) {
 
   // A copy is expected for non-const ArrayRef (thin copy)
   ArrayRef<int> ar1(kA1);
-  const ArrayRef<int> &ar1_ref = ArrayRef(ar1);
+  const ArrayRef<int> &ar1_ref = ArrayRef<int>(ar1);
   EXPECT_NE(&ar1, &ar1_ref);
   EXPECT_TRUE(ar1.equals(ar1_ref));
 

CTSRD-CHERI/compiler-rt

lib/sanitizer_common/tests/sanitizer_array_ref_test.cpp
@@ -109,13 +109,6 @@ TEST(ArrayRefTest, ConstConvert) {
   a = ArrayRef<int *>(ptrs);
 }
 
-static std::vector<int> ReturnTest12() { return {1, 2}; }
-static void ArgTest12(ArrayRef<int> a) {
-  EXPECT_EQ(2U, a.size());
-  EXPECT_EQ(1, a[0]);
-  EXPECT_EQ(2, a[1]);
-}
-
 TEST(ArrayRefTest, ArrayRef) {
   static const int kA1[] = {1, 2, 3, 4, 5, 6, 7, 8};
 
@@ -127,7 +120,7 @@ TEST(ArrayRefTest, ArrayRef) {
 
   // A copy is expected for non-const ArrayRef (thin copy)
   const ArrayRef<int> ar2(kA1);
-  const ArrayRef<int> &ar2_ref = ArrayRef(ar2);
+  const ArrayRef<int> &ar2_ref = ArrayRef<int>(ar2);
   EXPECT_NE(&ar2_ref, &ar2);
   EXPECT_TRUE(ar2.equals(ar2_ref));
 }